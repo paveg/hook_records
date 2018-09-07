4ac5eb23e657e7bc86f1175dc6a3c47de4b2d9b2
Ryota Ikezawa <pavegy@gmail.com>
2018-09-08 01:35:37 +0900

    More frontend setting

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module HookRecords
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators { |g| g.assets false }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
